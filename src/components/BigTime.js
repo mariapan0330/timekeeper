486e0fb0e196a8f72548ecc20f691320fbe992f4
Maria Panagos <maria.pan0330@gmail.com>
2023-12-08 12:27:14 -0600

    cleared warnings, replaced images w icons

--- src/components/BigTime.js
+++ src/components/BigTime.js
@@ -36,15 +36,13 @@ export default function BigTime() {
       <div className="flex justify-center items-center w-1/3">
         {/* col 1: up arrow, hours, down arrow */}
         <div className="flex flex-col">
-          <img
-            src={require("../images/up.png")}
-            className="w-10 self-center"
+          <i
+            className="fa-solid fa-angle-up text-6xl"
             onClick={() => setTimeInSeconds((t) => t + 3600)}
           />
           <div className="text-8xl font-bold py-4">{hours}</div>
-          <img
-            src={require("../images/up.png")}
-            className="w-10 self-center rotate-180"
+          <i
+            className="fa-solid fa-angle-down text-6xl"
             onClick={() => setTimeInSeconds((t) => (t - 3600 > 0 ? t - 3600 : t))}
           />
         </div>
@@ -54,15 +52,13 @@ export default function BigTime() {
 
         {/* col 2: up arrow, minutes, down arrow */}
         <div className="flex flex-col">
-          <img
-            src={require("../images/up.png")}
-            className="w-10 self-center"
+          <i
+            className="fa-solid fa-angle-up text-6xl"
             onClick={() => setTimeInSeconds((t) => t + 60)}
           />
           <div className="text-8xl font-bold py-4">{minutes}</div>
-          <img
-            src={require("../images/up.png")}
-            className="w-10 self-center rotate-180"
+          <i
+            className="fa-solid fa-angle-down text-6xl"
             onClick={() => setTimeInSeconds((t) => (t - 60 > 0 ? t - 60 : t))}
           />
         </div>
@@ -72,35 +68,31 @@ export default function BigTime() {
 
         {/* col 3: up arrow, seconds, down arrow */}
         <div className="flex flex-col">
-          <img
-            src={require("../images/up.png")}
-            className="w-10 self-center"
+          <i
+            className="fa-solid fa-angle-up text-6xl"
             onClick={() => setTimeInSeconds((t) => t + 1)}
           />
           <div className="text-8xl font-bold py-4">{seconds}</div>
-          <img
-            src={require("../images/up.png")}
-            className="w-10 self-center rotate-180"
+          <i
+            className="fa-solid fa-angle-down text-6xl"
             onClick={() => setTimeInSeconds((t) => (t - 1 > 0 ? t - 1 : t))}
           />
         </div>
       </div>
 
       {/* play/pause */}
-      <div className="pt-10 h-20" onClick={() => setIsRunning((prev) => !prev)}>
-        <img
-          src={isRunning ? require("../images/pause.png") : require("../images/play.png")}
-          className="w-20 self-center hover:drop-shadow-xl active:w-16"
-        />
+      <div className="pt-10 pb-32 h-20" onClick={() => setIsRunning((prev) => !prev)}>
+        {isRunning ? (
+          <i className="fa-solid fa-pause text-6xl hover:text-slate-600 hover:text-7xl active:text-5xl w-20 h-20 flex justify-center items-center" />
+        ) : (
+          <i className="fa-solid fa-play text-6xl hover:text-slate-600 hover:text-7xl hover:cursor-pointer active:text-5xl w-20 h-20 flex justify-center items-center" />
+        )}
         <p>{isRunning ? "Running..." : "Paused."}</p>
       </div>
 
       {/* reset */}
-      <div className="pt-20 h-20" onClick={resetTimer}>
-        <img
-          src={require("../images/reset.png")}
-          className="w-10 self-center hover:drop-shadow-xl active:w-8"
-        />
+      <div className="h-20" onClick={resetTimer}>
+        <i className="fa-solid fa-rotate-left text-4xl hover:cursor-pointer hover:text-red-600 hover:text-5xl" />
       </div>
     </div>
   );
